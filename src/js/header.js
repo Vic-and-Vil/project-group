27a36db94c820bccde6398a3562dff22fedb6ea9
Vic <waleria.tuszkowa@gmail.com>
2025-08-08 22:35:34 +0200

    зміни

--- src/js/header.js
+++ src/js/header.js
@@ -11,3 +11,34 @@
     mobileMenu.classList.remove('is-open');
   });
 })();
+openMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+  document.body.style.overflow = 'hidden'; // додати
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+  document.body.style.overflow = ''; // додати
+});
+document.addEventListener('click', (e) => {
+  if (
+    mobileMenu.classList.contains('is-open') &&
+    !mobileMenu.contains(e.target) &&
+    !openMenuBtn.contains(e.target)
+  ) {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  }
+});
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  }
+});
+document.querySelectorAll('.mob-menu-item-link').forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  });
+});
